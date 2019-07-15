3c7cc927cb7cc7e18312c1fd5cb91453b4683c4c
Márk Farkas <mark.farkas@dorsum.eu>
2019-07-15 11:13:54 +0200

    increase version of dependencies; modify webpack config; removed unnecessary files

--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -1,7 +1,6 @@
-import "babel-polyfill";
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./component/App";
+import App from "./App";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
